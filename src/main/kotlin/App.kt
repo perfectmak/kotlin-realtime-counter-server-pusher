bb057323a52f34933faec1cd530b7e9e3cf24f51
Perfect Makanju <damiperfect@gmail.com>
2017-09-09 02:34:29 +0100

    Simplify directory structure

---
+++ src/main/kotlin/App.kt
@@ -0,0 +1,48 @@
+
+external fun require(module: String): dynamic
+
+external class JSON {
+    companion object {
+        fun stringify(value: Any): String
+    }
+}
+
+@JsModule("pusher")
+external class Pusher(config: dynamic) {
+    fun trigger(channel: String, event: String, data: dynamic)
+}
+
+
+val express = require("express")
+
+val pusherConfig  = object {
+    val appId = "YOUR_PUSHER_APP_ID"
+    val key = "YOUR_PUSHER_KEY"
+    val secret = "YOUR_PUSHER_SECRET"
+    val cluster = "YOUR_PUSHER_APP_CLUSTER"
+    val encrypted = true
+}
+
+val clickChannel = "click-channel"
+val clickEvent = "click-event"
+var currentClickCount = 0
+
+fun main(args: Array<String>) {
+    val app = express()
+    val pusher = Pusher(pusherConfig)
+    
+    app.get("/counts", { _, res ->
+        res.json(ClickCount(currentClickCount))
+    })
+
+    app.post("/clicks", { _, res ->
+        currentClickCount++
+        // broadcast new ClickCount
+        pusher.trigger(clickChannel, clickEvent, ClickCount(currentClickCount))
+        res.status(200).send()
+    })
+    
+    app.listen(9999, {
+        println("Listening on port 9999")
+    })
+}
